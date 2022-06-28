4733e331f5f93a4e99445e490f5e028d00914e03
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-28 22:38:28 +0530

    Car Damage?grey:black

--- car.js
+++ car.js
@@ -10,6 +10,7 @@ class Car{
         this.maxSpeed=3;
         this.friction=0.05;
         this.angle=0;
+        this.damaged=false;
 
         this.sensor=new Sensor(this);
         this.controls=new Controls();
@@ -18,7 +19,17 @@ class Car{
     update(roadBorders){
         this.#move();
         this.polygon=this.#createPolygon();
+        this.damaged=this.#assessDamage(roadBorders);
         this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersection(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
     }
 
     #createPolygon(){
@@ -86,6 +97,11 @@ class Car{
     }
 
     draw(ctx){
+        if(this.damaged){
+            ctx.fillStyle="grey";
+        }else{
+            ctx.fillStyle="black"
+        }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
         for(let i=1;i<this.polygon.length;i++){

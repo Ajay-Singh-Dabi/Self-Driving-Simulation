67623d8909015b79d34c2acaec3e4b5ec996cf91
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-29 00:20:49 +0530

    Getting the best car and storing to local

--- car.js
+++ car.js
@@ -121,7 +121,7 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx,color){
+    draw(ctx,color,drawSensor=false){
         if(this.damaged){
             ctx.fillStyle="gray";
         }else{
@@ -134,7 +134,7 @@ class Car{
         }
         ctx.fill();
 
-        if(this.sensor){
+        if(this.sensor && drawSensor){
             this.sensor.draw(ctx);
         }
     }

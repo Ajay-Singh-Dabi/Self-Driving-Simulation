fd4911a94bc65a7c4d1868b5233db1da9434f2c5
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-28 19:08:37 +0530

    Rotating Car in circle

--- car.js
+++ car.js
@@ -9,6 +9,7 @@ class Car{
         this.acceleration=0.2;
         this.maxSpeed = 3;
         this.friction=0.05;
+        this.angle =0;
 
         this.controls = new Controls();
     }
@@ -38,17 +39,31 @@ class Car{
         if(Math.abs(this.speed)<this.friction){
             this.speed =0 ;
         }
+
+        if(this.controls.left){
+            this.angle+=0.03;
+        }
+        if(this.controls.right){
+            this.angle-=0.03;
+        }
         this.y-=this.speed;
     }
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+        
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }

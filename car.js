1fe2f1e26c52cf74404250a68d514afc92369907
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-28 22:48:11 +0530

    Added Dummy

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -7,13 +7,13 @@ class Car{
 
         this.speed=0;
         this.acceleration=0.2;
-        this.maxSpeed=3;
+        this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
         this.damaged=false;
 
         this.sensor=new Sensor(this);
-        this.controls=new Controls();
+        this.controls=new Controls(controlType);
     }
 
     update(roadBorders){

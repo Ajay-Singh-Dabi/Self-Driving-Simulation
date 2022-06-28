67894377d7270d4ddb3ca7a1a604e73d2599e6bf
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-29 01:16:05 +0530

    Finalizing the model

--- car.js
+++ car.js
@@ -139,15 +139,23 @@ class Car{
     }
 
     draw(ctx,drawSensor=false){
+
+        if(this.sensor && drawSensor){
+            this.sensor.draw(ctx);
+        }
+        
         ctx.save();
         ctx.translate(this.x,this.y);
         ctx.rotate(-this.angle);
-        ctx.drawImage(this.mask,
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height);
-        ctx.globalCompositeOperation="multiply";
+
+        if(!this.damaged){
+            ctx.drawImage(this.mask,
+                -this.width/2,
+                -this.height/2,
+                this.width,
+                this.height);
+            ctx.globalCompositeOperation="multiply";
+        }
 
 
         ctx.drawImage(this.img,
@@ -158,8 +166,6 @@ class Car{
     
         ctx.restore();
 
-        if(this.sensor && drawSensor){
-            this.sensor.draw(ctx);
-        }
+        
     }
 }
